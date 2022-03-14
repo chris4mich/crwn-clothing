9bc5628f88dc657fb9caeef89a3b49c4c2658073
Chris Mich <chrs.mich@gmail.com>
2022-03-14 21:41:13 +0200

    updating our directory page with directory component and category item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,57 +1,14 @@
-import React from "react";
-import MenuItem from "../menu-item/menu-item.component";
+import CategoryItem from "../category-item/category-item.components";
 import './directory.styles.scss';
 
-class Directory extends React.Component{
-    constructor() {
-        super();
-        this.state = {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1,
-                    linkUrl: 'shop/hats'
-                  },
-                  {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2,
-                    linkUrl: 'shop/jackets'
-                  },
-                  {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3,
-                    linkUrl: 'shop/sneakers'
-                  },
-                  {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                    size: 'large',
-                    id: 4,
-                    linkUrl: 'shop/womens'
-                  },
-                  {
-                    title: 'mens',
-                    imageUrl: 'https://arte8lusso.net/wp-content/uploads/2020/07/andreasweinas_20200621_162837_0-820x1024.jpg',
-                    size: 'large',
-                    id: 5,
-                    linkUrl: 'shop/mens'
-                  }]
-        }
-    }
-    render(){
-        return (
-        <div className="directory-menu">
-        {
-            this.state.sections.map(({ title, imageUrl, id, size}) => (
-                <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-            ))
-        }
-        </div>
-        )
-    }
-}
+const Directory = ({categories}) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
 
 export default Directory;
